c45ff1ae3a211f64aab2a7be61d2c159c2f5693a
scalgary92 <sandrine.lebon@ipsos.com>
2023-06-15 09:21:24 -0600

    Add first function check_dataframe

---
+++ R/check_dataframe.R
@@ -0,0 +1,39 @@
+check_dataframe <- function(df) {
+  check <- TRUE
+  # Vérifie si l'argument est une dataframe
+  if (!is.data.frame(df)) {
+    check <- FALSE
+    stop("Argument needs to be a dataframe")
+  }
+
+  # Vérifie si la dataframe a au moins 3 lignes
+  if (nrow(df) < 3) {
+    check <- FALSE
+    stop("dataframe needs to have at least 3 rows")
+  }
+
+  # Vérifie si la dataframe a au moins 3 colonnes
+  if (ncol(df) < 3) {
+    check <- FALSE
+    stop("dataframe needs to have at least 3 columns")
+  }
+
+  # Vérifie si toutes les lignes ont des noms
+  if (any(is.na(rownames(df)))) {
+    check <- FALSE
+    stop("Missing Attributes names")
+  }
+
+  # Vérifie si toutes les valeurs de la dataframe sont numériques
+  if (!all(sapply(df, is.numeric))) {
+    check <- FALSE
+    stop("Non numeric values detected")
+  }
+
+  # Vérifie s'il y a des valeurs manquantes
+  if (any(is.na(df))) {
+    check <- FALSE
+    stop("Missing values detected")
+  }
+  return(check)
+}
