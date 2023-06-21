39bfb9342cd0cddb1e8c27d3e6375ce414e7b35f
scalgary92 <sandrine.lebon@ipsos.com>
2023-06-20 23:24:43 -0600

    helper function

--- inst/Rcode/helper_functions.R
+++ inst/Rcode/helper_functions.R
@@ -1,36 +1,37 @@
 ##helper function to get 1 pptx with all the quadmaps
-add_slide_pptx <- function(doc_pptx, i) {
 
 
-  doc_pptx %>%
-    officer::add_slide(layout = "Title and Content", master = "Office Theme") %>%
-    officer::ph_with(names(plots)[i], location= officer::ph_location_type(type = "title")) %>%
-    officer::ph_with(rvg::dml(ggobj = plots[[i]]),
-                     location=officer::ph_location_type(type="body"))
-}
-add_slide_pptx_template <- function(doc_pptx, i) {
+#mylist is a list of plots
+custom_pptx <- function(mylist,use_template = TRUE) {
 
+  #check if argumnet 1 is a list
+  if (!(inherits(mylist, "list"))) {stop("You need a list of plots")}
+  all_ggplot <- sapply(mylist, function(x) inherits(x, "ggplot"))
+  if (!(all(all_ggplot))) {
+    stop("Not all elements are of class ggplot.")
+  }
 
-  doc_pptx %>%
-    officer::add_slide(layout = "OnlyTitle", master = "Custom Design") %>%
-    officer::ph_with(names(plots)[i], location= ph_location_label(ph_label = "Title 4")) %>%
-    officer::add_slide(layout = "OnlyTitle", master = "Custom Design")  %>%
-    officer::ph_with(names(plots)[i], location= ph_location_label(ph_label = "Title 4")) %>%
-    officer::add_slide(layout = "TitleContent", master = "Custom Design") %>%
-    officer::ph_with(paste0(names(plots)[i]," - Leveraging  Strengths and Weaknesses")
-                     , location= ph_location_label(ph_label = "Title 4")) %>%
-    officer::ph_with(rvg::dml(ggobj = plots[[i]]),
-                     location=ph_location_label(ph_label = "Content Placeholder 2"))
+  all_have_names <- !any(is.null(names(mylist)))
 
-
-
-}
-custom_reduce <- function(mylist,use_template) {
+  if (!(all_have_names)) {
+    stop("All elements should have names.")
+  }
   if (use_template){
     init_pptx <- officer::read_pptx(system.file(package = "HelpMe", "template/templateGG.pptx"))
-
+    add_slide_pptx_template <- function(doc_pptx, i, myplots = mylist) {
+      doc_pptx %>%
+        officer::add_slide(layout = "OnlyTitle", master = "Custom Design") %>%
+        officer::ph_with(names(myplots)[i], location= ph_location_label(ph_label = "Title 4")) %>%
+        officer::add_slide(layout = "OnlyTitle", master = "Custom Design")  %>%
+        officer::ph_with(names(myplots)[i], location= ph_location_label(ph_label = "Title 4")) %>%
+        officer::add_slide(layout = "TitleContent", master = "Custom Design") %>%
+        officer::ph_with(paste0(names(myplots)[i]," - Leveraging  Strengths and Weaknesses")
+                         , location= ph_location_label(ph_label = "Title 4")) %>%
+        officer::ph_with(rvg::dml(ggobj = myplots[[i]]),
+                         location=ph_location_label(ph_label = "Content Placeholder 2"))
+    }
     # Votre objet initial
-    result <- Reduce(add_slide_pptx_template, mylist, init = init_pptx)
+    result <- Reduce(add_slide_pptx_template, seq_along(mylist), init = init_pptx)
     result <- result %>%
       officer::add_slide(layout = "OnlyTitle", master = "Custom Design") %>%
       officer::ph_with("Appendix", location= ph_location_label(ph_label = "Title 4")) %>%
@@ -39,7 +40,14 @@ custom_reduce <- function(mylist,use_template) {
 
   } else {
     init_pptx <- officer::read_pptx()  # Votre objet initial
-    result <- Reduce(add_slide_pptx, mylist, init = init_pptx)
+    add_slide_pptx <- function(doc_pptx, i, myplots = mylist) {
+      doc_pptx %>%
+        officer::add_slide(layout = "Title and Content", master = "Office Theme") %>%
+        officer::ph_with(names(myplots)[i], location= officer::ph_location_type(type = "title")) %>%
+        officer::ph_with(rvg::dml(ggobj = myplots[[i]]),
+                         location=officer::ph_location_type(type="body"))
+    }
+    result <- Reduce(add_slide_pptx, seq_along(mylist), init = init_pptx)
 
   }
 
