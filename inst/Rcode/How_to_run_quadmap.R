6021e57c388b29b32c9e1fb37b1a9e8a20f54f46
Sandrine Lebon <Sandrine.Lebon@ipsos.com>
2023-06-21 11:17:38 -0600

    Rcode file

--- inst/Rcode/How_to_run_quadmap.R
+++ inst/Rcode/How_to_run_quadmap.R
@@ -3,39 +3,30 @@ library(HelpMe)
 
 ###############Template QuadMap###############
 
-file_csv <- "dataquad.csv" #your file 2n +1 columns
+file_csv <- "dataquad.csv"
+file_csv <- system.file(package = "HelpMe", "extdata/dataquad.csv")
+
+#your file 2n +1 columns
 #1st column label
 # 2 to n+1 Performance
 #n+2 to 2n+1 impact
 #columns impact give name quadmap
 
-plots <- HelpMe::run_quadmap(file_csv)
+plots <- HelpMe::run_quadmap(file_csv, TBdetail = "T2B")
 
 #visualisation
 plots
 
 ###export to pptx needs officer and rvg
-library(officer)
-library(rvg)
 ##helper function to get 1 pptx with all the quadmaps
+#helper functions not in package as depend of ooficer and rbg
+source(system.file(package = "HelpMe", "Rcode/helper_functions.R"))
+##helper function to get 1 pptx with all the quadmaps
+#helper functions not in package as depend of ooficer and rbg
 
-add_slide_pptx <- function(doc_pptx, i) {
+plots <- HelpMe::run_quadmap(file_csv, TBdetail = "T2B")
+print(custom_pptx(plots),"with_template_T2B.pptx")
 
-    doc_pptx %>%
-    officer::add_slide(layout = "Title and Content", master = "Office Theme") %>%
-    officer::ph_with(names(plots)[i], location= officer::ph_location_type(type = "title")) %>%
-    officer::ph_with(rvg::dml(ggobj = plots[[i]]),
-                     location=officer::ph_location_type(type="body"))
-}
-#######
-
-
-init_pptx <- officer::read_pptx()  # Votre objet initial
-result <- Reduce(add_slide_pptx, seq_along(plots), init = init_pptx)
-print(result, target ="new.pptx")
-
-
-
-
-
+plots <- HelpMe::run_quadmap(file_csv, TBdetail = "TB")
+print(custom_pptx(plots),"with_template_TB.pptx")
 
