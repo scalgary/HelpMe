bae964f472536f084d79c47edaf1c2e984a1e1d3
scalgary92 <sandrine.lebon@ipsos.com>
2023-06-25 18:31:19 -0600

    Work with vignette

--- R/check_pptx.CA.R
+++ R/check_pptx.CA.R
@@ -10,7 +10,14 @@ check_pptx.CA <- function() {
 
   check <- FALSE
   required_packages <- c("officer", "rvg")
+  # Check if all required packages are installed
+  if (all(sapply(required_packages, function(p) require(p, character.only = TRUE, quietly = TRUE)))) {
+    # All packages are installed
+    print("All additional required packages are installed.")
+    print("No warranty - should work with officer 0.6.2 and rvg 0.3.3")
 
+    check <- TRUE
+  }
 
   if (check) {
     source(system.file(package = "HelpMe", "Rcode/helper_functions.R"))
