79e416ef1640570385d06ed49e582f5997b4e9bc
Sandrine <sandrine.lebon@ipsos.com>
2023-07-28 17:29:04 -0600

    good quadmap

--- inst/Rcode/helper_functions.R
+++ inst/Rcode/helper_functions.R
@@ -6,7 +6,7 @@ library(rvg)
 
 
 #mylist is a list of plots
-save_quadmap_pptx <- function(mylist, target  ) {
+save_quadmap_pptx_old <- function(mylist, target  ) {
 
   #check if argumnet 1 is a list
   if (!(inherits(mylist, "list"))) {stop("You need a list of plots")}
@@ -47,7 +47,7 @@ save_CA_pptx <- function(x, target, folder = NULL, max.overlaps = 10, usetemplat
     mytitle <-  paste0(toupper(substr(string, 1, 1)), substr(string, 2, nchar(string)))
     if (is.null(target)) { target <- paste0(res.ca$title,".pptx")}
     #si user forgot pptx
-    if (!(grepl("\\.pptx$", target))) {paste0(target,".pptx")}
+    if (!(grepl("\\.pptx$", target))) {target <- paste0(target,".pptx")}
   if (usetemplate) {
     doc_pptx <-officer::read_pptx(system.file(package = "HelpMe", "template/templateISC.pptx")) %>%
       officer::add_slide(layout = "TitleContent", master = "Custom Design") %>%
@@ -67,3 +67,52 @@ save_CA_pptx <- function(x, target, folder = NULL, max.overlaps = 10, usetemplat
   print(doc_pptx, target)}
     else {print(doc_pptx, file.path(folder,target))}
 }
+
+
+
+#mylist is a list of plots
+save_quadmap_pptx <- function(mylist, target, folder = NULL) {
+
+  #check if argumnet 1 is a list
+  if (!(inherits(mylist, "list"))) {stop("You need a list of plots")}
+  all_ggplot <- sapply(mylist, function(x) inherits(x, "ggplot"))
+  if (!(all(all_ggplot))) {
+    stop("Not all elements are of class ggplot.")
+  }
+
+  all_have_names <- !any(is.null(names(mylist)))
+
+  if (!(all_have_names)) {
+    stop("All elements should have names.")
+  }
+
+  init_pptx <- officer::read_pptx(system.file(package = "HelpMe", "template/templateGG.pptx"))
+  add_slide_pptx_template <- function(doc_pptx, i, myplots = mylist) {
+    doc_pptx %>%
+      officer::add_slide(layout = "OnlyTitle", master = "Custom Design") %>%
+      officer::ph_with(names(myplots)[i], location= ph_location_label(ph_label = "Title 4")) %>%
+      officer::add_slide(layout = "OnlyTitle", master = "Custom Design")  %>%
+      officer::ph_with(names(myplots)[i], location= ph_location_label(ph_label = "Title 4")) %>%
+      officer::add_slide(layout = "TitleContent", master = "Custom Design") %>%
+      officer::ph_with(paste0(names(myplots)[i]," - Leveraging  Strengths and Weaknesses")
+                       , location= ph_location_label(ph_label = "Title 4")) %>%
+      officer::ph_with(rvg::dml(ggobj = myplots[[i]]),
+                       location=ph_location_label(ph_label = "Content Placeholder 2"))
+  }
+  # Votre objet initial
+  result <- Reduce(add_slide_pptx_template, seq_along(mylist), init = init_pptx)
+  result <- result %>%
+    officer::add_slide(layout = "OnlyTitle", master = "Custom Design") %>%
+    officer::ph_with("Appendix", location= ph_location_label(ph_label = "Title 4")) %>%
+    officer::add_slide(layout = "OnlyTitle", master = "Custom Design") %>%
+    officer::ph_with("Appendix", location= ph_location_label(ph_label = "Title 4"))
+
+  if (!(grepl("\\.pptx$", target))) {target <- paste0(target,".pptx")}
+  if (is.null(folder)) {
+    print(result, target)}
+  else {print(result, file.path(folder,target))}
+
+
+
+}
+
