486b0ea8016d9b1d5477a2c19d09700ca911ec89
Sandrine <sandrine.lebon@ipsos.com>
2023-10-24 13:44:55 -0600

    add some stuff

---
+++ inst/Rcode/package_cmd.R
@@ -0,0 +1,12 @@
+library("devtools")
+
+
+load_all()
+document()
+devtools::build_rmd("vignettes/how_to_Perceptual_Maps.Rmd")
+devtools::build_rmd("vignettes/How_to_Quadmap.Rmd")
+
+check()
+build()
+remove.packages("HelpMe")
+install.packages("~/SAS_MS/HelpMe_2.1.6.tar.gz", repos = NULL, type = "source")
