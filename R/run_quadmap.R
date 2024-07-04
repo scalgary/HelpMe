368c5929fd730d7f8712f80c34e5a132b138ab27
sandrine <sandrine.lebon@ipsos.com>
2024-07-04 11:57:09 -0600

    change a bit quadmap for BG

--- R/run_quadmap.R
+++ R/run_quadmap.R
@@ -21,3 +21,13 @@ run_quadmap <- function(file_csv, folder= NULL, isHE = FALSE, TBdetail = "T2B",
   plots <- stats::setNames(plots, sapply(df_list, function(df) utils::tail(names(df), n = 1)))
   return(plots)
 }
+
+run_quadmap_BG <- function(file_csv, folder= NULL, isHE = FALSE, TBdetail = "T2B",
+                        masterbrand = NULL, force = 10, max.overlaps = 10){
+  df <- read.file(file_csv, folder)
+  df_list <- split_data_frame(df)
+  plots <- lapply(df_list, function(df) myplot_quadmap_BG(df, isHE, masterbrand, force, TBdetail,max.overlaps ))
+  # Set the names of the list elements based on the last column of each dataframe
+  plots <- stats::setNames(plots, sapply(df_list, function(df) utils::tail(names(df), n = 1)))
+  return(plots)
+}
