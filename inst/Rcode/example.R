75db67ef3cb5b50cff016b78e2bd03acdc6f6ce9
sandrine <sandrine.lebon@ipsos.com>
2024-07-08 13:24:08 -0600

    Update quadmap for BG

--- inst/Rcode/example.R
+++ inst/Rcode/example.R
@@ -3,11 +3,16 @@ devtools::load_all()
 devtools::document()
 devtools::check()
 devtools::build()
+
+
 options(ggrepel.max.overlaps = Inf)
-setwd("J:\\sas_ms\\24-BrandPulse-AR123\\FR")
+
+
+setwd("J:\\sas_ms\\24-BrandPulse-AR123\\ID")
 file <- list.files(pattern="quadmap")
+file
 myplotsBG <- run_quadmap_BG(file,TBdetail="T2B", max.overlaps = Inf)
 myplotsBG[["Love"]]
 
 
-if (HelpMe::check_pptx()) {save_quadmap_mypptx(myplotsBG,"FRtest.pptx")}
+if (HelpMe::check_pptx()) {save_quadmap_pptx(myplotsBG,"IDBGx.pptx")}
